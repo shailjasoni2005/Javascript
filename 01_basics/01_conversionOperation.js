203630503d4486d76aa26c9661dd7d8e752277da
shailjasoni2005 <144000431+shailjasoni2005@users.noreply.github.com>
2024-04-19 16:11:15 +0000

    conversion of data types

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,22 @@
+// let score="33a";
+// let score=null;
+let score=undefined;
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+we can convert 33 but if it becomes 33abd like this then its nan i.e. it does'nt converted  
+*/
+
+let IsLoggedIn=1;
+let booleanIsLoggedIn=Boolean(IsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber=33;
+console.log(someNumber);
+let stringNumber=String(someNumber);
+console.log(stringNumber); 
+console.log(typeof stringNumber); 
