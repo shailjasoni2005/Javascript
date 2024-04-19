ca6adb39d4ed91d29019a27359404f18f0052123
shailjasoni2005 <144000431+shailjasoni2005@users.noreply.github.com>
2024-04-19 14:34:33 +0000

    datatype

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";    //treat all js code as newer version
+//alert(3+3)   //We are using nodejs, not browser
+let name="shailja";
+let id=176;
+let age=18;
+let isLogedIn="false";
+
+
+//Primitive data types:
+
+/*
+number => 2 to power 53
+bigint => for larger no in any bid website 
+string
+boolean =>either true or false
+null = it is standalone value (it ia empty)
+undefined => the variable is declared but have no value and is not defined
+symbol => use to find unique element
+
+*/
+
+//object
+
+console.log(typeof null);    //type->object
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId=1234;
+let accountEmail="shailja123@gmail.com";
+var accountPassword= "####";
+ accountCity="bhopal";
+console.log(accountEmail);
+// Do not use var due to issue in block scope and functional scope
